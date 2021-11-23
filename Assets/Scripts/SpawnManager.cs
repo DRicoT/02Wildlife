f8d99d91942e4391c7af9892ec13f93827af3f1c
DRicoT <wasnaisxp@gmail.com>
2021-11-23 20:13:17 +0100

    practica_23/11

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,37 +8,48 @@ using Random = UnityEngine.Random;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] enemies;
-    public int animalIndex;
+    private int animalIndex;
 
-    private float spawnRangeX = 20.0f;
+    private float spawnRangeX = 20f;
     private float spawnPosZ;
-
+    
+    [SerializeField, Range(2,5)]
+    private float startDelay = 2f;
+    [SerializeField, Range(1,5)]
+    private float spawnInterval = 1.5f;
+    
     private void Start()
     {
         spawnPosZ = this.transform.position.z;
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        //ESTO SOLO ES UNA PRUEBA
+
         /*if (animalIndex >= 0 && animalIndex < enemies.Length)
-        {*/
+        {
             if (Input.GetKeyDown(KeyCode.R))
             {
-                //Generar la posición donde aparecerán los enemigos
-                Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX), 0, spawnPosZ);
-                animalIndex = Random.Range(0, enemies.Length);
-               
-                Instantiate(enemies[animalIndex],spawnPos,
-                    enemies[animalIndex].transform.rotation);
+                SpawnRandomAnimal();
             }
             //El animal existe
-        /*}
-        else
-        {
-            Debug.LogError("El número de instanciamiento NO es correcto");
-        }*/
+            }
+            else
+            {
+                Debug.LogError("El número de instanciamiento NO es correcto");
+            }*/
+    }
+
+    private void SpawnRandomAnimal()
+    {
+        //Generar la posición donde aparecerán los enemigos
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX), 0, spawnPosZ);
+        animalIndex = Random.Range(0, enemies.Length);
+               
+        Instantiate(enemies[animalIndex],spawnPos,
+            enemies[animalIndex].transform.rotation);
     }
 }
